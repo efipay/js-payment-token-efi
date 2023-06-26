f2153c613cfeae55dfb9f115ba0c412fe702a7c3
guilherme-cota-efi <121238882+guilherme-cota-efi@users.noreply.github.com>
2023-06-26 16:21:09 -0300

    ➕docs: Exemplo de utilização em Node

---
+++ distNode/example.js
@@ -0,0 +1,38 @@
+const EfiJs = require('payment-token-efi');
+
+try {
+	EfiJs.CreditCard
+		.setAccount('Identificador_de_conta_aqui')
+		.setEnvironment('production') // 'production' or 'homologation'
+		.setCreditCardData({
+			brand: 'visa',
+			number: '4485785674290087',
+			cvv: '123',
+			expirationMonth: '05',
+			expirationYear: '2029',
+			reuse: false
+		})
+		.getPaymentToken()
+		.then(data => {
+			const payment_token = data.payment_token;
+			const card_mask = data.card_mask;
+			console.log('payment_token', payment_token);
+			console.log('card_mask', card_mask);
+		}).catch(err => {
+			if (err.code) {
+				console.log('Código: ', err.code);
+				console.log('Nome: ', err.error);
+				console.log('Mensagem: ', err.error_description);
+			} else {
+				console.log(err);
+			}
+		});
+} catch (error) {
+	if (error.code) {
+		console.log('Código: ', error.code);
+		console.log('Nome: ', error.error);
+		console.log('Mensagem: ', error.error_description);
+	} else {
+		console.log(err);
+	}
+}
